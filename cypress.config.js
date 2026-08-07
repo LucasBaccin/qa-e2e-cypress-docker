df547170ea1c7d89876c8cdb3f456b7986178bce
Lucas Baccin <lbaccin1998@gmail.com>
2026-08-07 00:58:26 -0300

    Ajustes no código para implementar o Allure e o Cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,29 @@
-const { defineConfig } = require('cypress')
-const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
-const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor')
-const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild')
-const { allureCypress } = require('allure-cypress/reporter')
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
+const { allureCypress } = require('allure-cypress/reporter');
+const cypressOnFixModule = require('cypress-on-fix');
+const cypressOnFix = cypressOnFixModule.default || cypressOnFixModule;
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      allureCypress(on, config)
-      return config
-    },
-    specPattern: 'cypress/e2e/**/*.feature',
     async setupNodeEvents(on, config) {
-      await addCucumberPreprocessorPlugin(on, config)
+      on = cypressOnFix(on);
+
+      await addCucumberPreprocessorPlugin(on, config);
+
       on('file:preprocessor', createBundler({
         plugins: [createEsbuildPlugin(config)]
-      }))
+      }));
+
+      allureCypress(on, config);
+
       return config
     },
+
+    specPattern: 'cypress/e2e/**/*.feature',
+    supportFile: 'cypress/support/e2e.js',
     baseUrl: 'http://localhost:3000/',
   },
 })
