0e6a26f315d3d071c3b450794c3b1d524e46d276
Lucas Baccin <lbaccin1998@gmail.com>
2026-08-07 00:24:27 -0300

    Ajuste e adição do allure no código fonte

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,14 @@ const { defineConfig } = require('cypress')
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
 const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor')
 const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild')
+const { allureCypress } = require('allure-cypress/reporter')
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      allureCypress(on, config)
+      return config
+    },
     specPattern: 'cypress/e2e/**/*.feature',
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config)
